b9db4a052d357b83d445bf616f4dbffaf424a6f9
George Foto <fotog@wit.edu>
2020-09-17 16:02:45 -0400

    Initial commit

---
+++ src/edu/wit/comp1050/TestMain.java
@@ -0,0 +1,8 @@
+package edu.wit.comp1050;
+
+public class TestMain {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+        System.out.println(1);
+    }
+}

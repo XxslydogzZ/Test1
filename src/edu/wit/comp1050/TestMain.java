ec65f9b2e30cd09d862b1646baa58e9418899693
George Foto <fotog@wit.edu>
2020-09-17 16:13:40 -0400

    edit

--- src/edu/wit/comp1050/TestMain.java
+++ src/edu/wit/comp1050/TestMain.java
@@ -4,5 +4,6 @@ public class TestMain {
     public static void main(String[] args) {
         System.out.println("Hello world");
         System.out.println(1);
+        System.out.println("change");
     }
 }
